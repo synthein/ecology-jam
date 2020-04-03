67d26cdabe31988e11f8dabe0e6a9a96a79c4c19
Jordan Christiansen <xordspar0@gmail.com>
2020-04-02 19:01:37 -0500

    Add a constant to expPlot.py

--- expPlot.py
+++ expPlot.py
@@ -9,13 +9,14 @@ def exponentialRandom():
 
 samples = []
 
-for i in range(1000):
+N = 1000
+for i in range(N):
     samples.append(exponentialRandom())
 
 fig, axs = plt.subplots(1, 1, sharey=True, tight_layout=True)
 
 axs.set_title("Clover spawning")
-axs.set_ylabel("Number of instances (N = 1000)")
+axs.set_ylabel("Number of instances (N = {})".format(N))
 axs.set_xlabel("Distance from parent")
 
 axs.hist(samples, bins=15)
